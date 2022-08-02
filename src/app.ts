034d9a5ca8b77de30d1d4fab18baa36ecfdf50ea
Fernando Carvalho <fernando.carvalho.iff@gmail.com>
2022-08-02 17:47:55 +0000

    Update app.ts and routes.ts

--- src/app.ts
+++ src/app.ts
@@ -5,49 +5,27 @@ import { ServerApiVersion } from "mongodb";
 // import UserController from './controllers/UserController';
 import routes from "./routes";
 // import cria_tabela from './infra/cria_tabelas';
+import Database from "./database";
 
 class App {
   public express: express.Application;
+  public db: Database;
 
   public constructor() {
-    this.express = express();
-    this.express.use(cors());
-
     this.middlewares();
-    this.initMongoose();
-    this.connectDatabase();
     this.routes();
-  }
-
-  private initMongoose() {
-    mongoose.set("runValidators", true);
   }
 
   private middlewares(): void {
     this.express = express();
     this.express.use(express.json());
     this.express.use(cors());
+    this.db = new Database();
+    this.db.connectDatabase();
   }
 
   private routes() {
     this.express.use(routes);
-  }
-
-  private connectDatabase(): void {
-    const uri =
-      "mongodb+srv://admin:admin@cluster0.c0lb9.mongodb.net/hotel?retryWrites=true&w=majority";
-    // const uri: string = "mongodb+srv://admin:admin@cluster0.orgtp6c.mongodb.net/primeiro?retryWrites=true&w=majority";
-    mongoose
-      .connect(uri, {
-        serverApi: ServerApiVersion.v1
-      })
-      .then(() => {
-        console.log("Conectado ao mongodb");
-        // cria_tabela()
-      })
-      .catch((error) => {
-        console.log(error);
-      });
   }
 
   public listen(port: number): void {
--- src/routes.ts
+++ src/routes.ts
@@ -1,37 +1,37 @@
-import { Router } from 'express';
-import hotelController from './controller/hotel';
-import quartoController from './controller/quarto';
-import telefoneController from './controller/telefone';
-import enderecoController from './controller/endereco';
+import { Router } from "express";
+// import hotelController from './controller/hotel';
+// import quartoController from './controller/quarto';
+// import telefoneController from './controller/telefone';
+import enderecoController from "./controller/endereco";
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-    return res.send('Hello World');
-})
+routes.get("/", (req, res) => {
+  return res.send("Hello World fernando");
+});
 
-routes.get('/hotels', hotelController.index);
-routes.get('/hotel/:id', hotelController.get);
-routes.put('/hotel/:id', hotelController.add);
-routes.delete('/hotel/:id', hotelController.delete);
-routes.post('/hotel/:id', hotelController.update);
+// routes.get('/hotels', hotelController.index);
+// routes.get('/hotel/:id', hotelController.get);
+// routes.put('/hotel/:id', hotelController.add);
+// routes.delete('/hotel/:id', hotelController.delete);
+// routes.post('/hotel/:id', hotelController.update);
 
-routes.get('/quartos', quartoController.index);
-routes.get('/quarto/:id', quartoController.get);
-routes.put('/quarto/:id', quartoController.add);
-routes.delete('/quarto/:id', quartoController.delete);
-routes.post('/quarto/:id', quartoController.update);
+// routes.get('/quartos', quartoController.index);
+// routes.get('/quarto/:id', quartoController.get);
+// routes.put('/quarto/:id', quartoController.add);
+// routes.delete('/quarto/:id', quartoController.delete);
+// routes.post('/quarto/:id', quartoController.update);
 
-routes.get('/telefones', telefoneController.index);
-routes.get('/telefone/:id', telefoneController.get);
-routes.put('/telefone/:id', telefoneController.add);
-routes.delete('/telefone/:id', telefoneController.delete);
-routes.post('/telefone/:id', telefoneController.update);
+// routes.get('/telefones', telefoneController.index);
+// routes.get('/telefone/:id', telefoneController.get);
+// routes.put('/telefone/:id', telefoneController.add);
+// routes.delete('/telefone/:id', telefoneController.delete);
+// routes.post('/telefone/:id', telefoneController.update);
 
-routes.get('/enderecos', enderecoController.index);
-routes.get('/endereco/:id', enderecoController.get);
-routes.put('/endereco/:id', enderecoController.add);
-routes.delete('/endereco/:id', enderecoController.delete);
-routes.post('/endereco/:id', enderecoController.update);
+routes.get("/enderecos", enderecoController.index);
+routes.get("/endereco/:id", enderecoController.get);
+routes.put("/endereco/:id", enderecoController.add);
+routes.delete("/endereco/:id", enderecoController.delete);
+routes.post("/endereco/:id", enderecoController.update);
 
 export default routes;
